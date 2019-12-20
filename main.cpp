a31d977cb0195e724cb9795b275443b8def63e0d
yousaf2018 <mahmoodyousaf975974@gmail.com>
2019-12-20 10:53:12 +0500

    This is now 100% woriking file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
+#include<bits/stdc++.h>
 
 using namespace std;
-
+//using  branch 1
 class Node
 {
 private:
@@ -74,6 +74,10 @@ class Avl
   int max(int a, int b);
   int get_bal(Node*);
   Node* left_rotation(Node*);
+  Node* right_rotation(Node*);
+  void help_to_remove(int);
+  Node* remove_node(Node*,int);
+  Node* minValueNode(Node*);
 };
 Avl::Avl()
 {
@@ -104,6 +108,19 @@ Node* Avl::help_add_node(Node *temp,int data)
         int bal=get_bal(temp);
         if(bal==-2&&data>temp->get_data()&&data>temp->get_right()->get_data())
                 temp=left_rotation(temp);
+        else if(bal==2&&data<temp->get_data()&&data<temp->get_left()->get_data())
+                temp=right_rotation(temp);
+        else if(bal==2&&data>temp->get_left()->get_data())
+        {
+            temp->set_left(left_rotation(temp->get_left()));
+            return right_rotation(temp);
+        }
+        else if(bal==-2&&data<temp->get_right()->get_data())
+        {
+            temp->set_right(right_rotation(temp->get_right()));
+            return left_rotation(temp);
+        }
+
         return temp;
  }
 void Avl::display()
@@ -146,8 +163,87 @@ Node* Avl::left_rotation(Node *temp)
     temp->set_right(temp3);
     temp2->set_height(1+max(temp2->get_height(temp2->get_left()),temp2->get_height(temp2->get_right())));
     temp->set_height(1+max(temp->get_height(temp->get_left()),temp->get_height(temp->get_right())));
-    cout << "We are done\n";
     return temp2;
+}
+Node* Avl::right_rotation(Node *temp)
+{
+    Node *temp2=temp->get_left();
+    Node *temp3=temp2->get_right();
+    temp2->set_right(temp);
+    temp->set_left(temp3);
+    temp2->set_height(1+max(temp2->get_height(temp2->get_left()),temp2->get_height(temp2->get_right())));
+    temp->set_height(1+max(temp->get_height(temp->get_left()),temp->get_height(temp->get_right())));
+    return temp2;
+}
+void Avl::help_to_remove(int input)
+{
+        root=remove_node(root,input);
+}
+Node* Avl::remove_node(Node *temp,int input)
+{
+    if (temp == NULL)
+        return temp;
+    if ( input < temp->get_data() )
+        root->set_left(remove_node(temp->get_left(),input));
+    else if( input > temp->get_data() )
+        temp->set_right(remove_node(temp->get_right(),input));
+    // This else will help to delete node
+    else
+    {
+        // With only one chil or no child
+        if( (temp->get_left() == NULL) ||
+            (temp->get_right() == NULL) )
+        {
+            Node *temp2 = temp->get_left() ? temp->get_left() : temp->get_right();
+
+            // No child case
+            if (temp2 == NULL)
+            {
+                temp2 = temp;
+                temp = NULL;
+            }
+            else // One child case
+            temp->set_data(temp2->get_data());
+            free(temp2);
+        }
+        else
+        {
+            Node* temp3 = minValueNode(temp->get_right());
+            temp->set_data(temp3->get_data());//overwrite first node most left node
+            temp->set_right(remove_node(temp->get_right(),temp3->get_data()));// delete node
+        }
+    }
+
+    // Tree have only one node
+    if (temp == NULL)
+    return temp;
+    temp->set_height(1+max(temp->get_height(temp->get_left()),temp->get_height(temp->get_right())));
+    int bal=get_bal(temp);
+        if(bal==-2&&get_bal(temp->get_right())<=0)
+                temp=left_rotation(temp);
+        else if(bal==2&&get_bal(temp->get_left())>=0)
+                temp=right_rotation(temp);
+        else if(bal==2&&get_bal(temp->get_left())<0)
+        {
+            temp->set_left(left_rotation(temp->get_left()));
+            return right_rotation(temp);
+        }
+        else if(bal==-2&&get_bal(temp->get_right())>0)
+        {
+            temp->set_right(right_rotation(temp->get_right()));
+            return left_rotation(temp);
+        }
+
+        return temp;
+}
+Node* Avl::minValueNode(Node* temp)
+{
+    Node* newone = temp;
+
+    while (newone->get_left() != NULL)
+        newone = newone->get_left();
+
+    return newone;
 }
 int main()
 {
@@ -158,8 +254,7 @@ int main()
                 cout << "****************************************\n";
                 cout << "Enter 1 for insert value in Avl tree\n";
                 cout << "Enter 2 for display\n";
-                cout << "Enter 3 for search value in Avl tree\n";
-                cout << "Enter 4 for remove value in Avl tree\n";
+                cout << "Enter 3 for remove value in Avl tree\n";
                 cout << "****************************************\n";
                 cin >> option;
                 if(option==1)
@@ -172,18 +267,11 @@ int main()
                 {
                         object.display();
                 }
-             /*  else if(option==3)
-                {
-                        cout << "Enter value you want to search\n";
-                        cin >> input;
-                        object.search_help(input);
-                }
-                else if(option==4)
+                else if(option==3)
                 {
                         cout << "Enter value you want to remove\n";
                         cin >> input;
                         object.help_to_remove(input);
                 }
-                */
         }
 }
